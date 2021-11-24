0fb14aa2fec92e3781868d77b1181a490a89cb1f
xricherenkova <79928904+xricherenkova@users.noreply.github.com>
2021-11-24 22:10:31 +0400

    Add files via upload

---
+++ Jenkins/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jenkins
+{
+    public class Number
+    {
+        public int GetNumber(int number)
+        {
+            return number;
+        }
+    }
+}
---
+++ Jenkins/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Jenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal = new Number();
+            Console.WriteLine(animal.GetNumber(7));
+            Console.ReadLine();
+        }
+    }
+}
