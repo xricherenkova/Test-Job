6a40d59829d1f2832be966f730bab75cdd4b417c
xricherenkova <79928904+xricherenkova@users.noreply.github.com>
2021-11-25 08:08:37 +0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-cd C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\
+cd C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
 MSBuild.exe C:\ProgramData\Jenkins\.jenkins\workspace\MyJob\Jenkins
