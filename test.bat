d4b4d34d38433ddb87eac4dcbd9009d2d5141b7d
xricherenkova <79928904+xricherenkova@users.noreply.github.com>
2021-11-25 08:21:38 +0400

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,2 @@
+cd C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow
+vstest.console.exe C:\ProgramData\Jenkins\.jenkins\workspace\MyJob\JenkinsMsTest\bin\Debug\netcoreapp2.1\JenkinsMsTest.dll
