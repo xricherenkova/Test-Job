bc7c61e2f45f80075deded58a701ca1c382cf597
xricherenkova <79928904+xricherenkova@users.noreply.github.com>
2021-12-03 16:30:55 +0400

    fix build

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 cd C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
 dotnet restore C:\ProgramData\Jenkins\.jenkins\workspace\MyJob\
 MSBuild.exe C:\ProgramData\Jenkins\.jenkins\workspace\MyJob\Jenkins /restore
+MSBuild.exe C:\ProgramData\Jenkins\.jenkins\workspace\MyJob\JenkinsMsTest /restore
