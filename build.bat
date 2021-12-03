18664a84606fb66504c5bf274256c4e0d307fba5
xricherenkova <79928904+xricherenkova@users.noreply.github.com>
2021-12-03 10:23:31 +0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 cd C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
 dotnet restore C:\ProgramData\Jenkins\.jenkins\workspace\MyJob\
-MSBuild.exe C:\ProgramData\Jenkins\.jenkins\workspace\MyJob\Jenkins
+MSBuild.exe C:\ProgramData\Jenkins\.jenkins\workspace\MyJob\Jenkins /restore
